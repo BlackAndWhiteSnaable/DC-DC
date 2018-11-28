21bf405004ebd8f841435fd1c9373345ab6dc07a
Ilian <ilianbg9@gmail.com>
2018-11-28 17:01:43 +0100

    CBC

--- Inverter/Report/Figure_generator.m
+++ Inverter/Report/Figure_generator.m
@@ -2,14 +2,13 @@
 figure('Name','Data','NumberTitle','off','Position',[10 100 1200 600])
 
 %%Change Name of variable of whatever you set it in Simulink
-subplot(2,1,1)
-plot(I_out,'Color',[1,0,0])
-%axis([0 0.25 0 60]) 
+plot(V_TLBC,'Color',[1,0,0])
+axis([0 0.25 0 60])
 %%Set title, labels, legend labels
-title('Simulated Output Current of BC','Interpreter', 'latex','fontsize',18)
+title('Simulated Output Voltage of TLBC','Interpreter', 'latex','fontsize',18)
 xlabel('Time[$s$]', 'Interpreter', 'latex','fontsize',18);
 ylabel('Voltage[$V$]', 'Interpreter', 'latex','fontsize',18);
-leg = legend('$I_ O$');
+leg = legend('$V_ O$');
 
 %%Irrlevant
 set(leg,'Interpreter','latex');

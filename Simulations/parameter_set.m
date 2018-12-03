4c47fb8a6ff0159c73c818c96bd9776be84e5e56
Ivan Kulin <ivankulin@gmail.com>
2018-12-03 16:19:01 +0100

    Figures for big one

--- Simulations/parameter_set.m
+++ Simulations/parameter_set.m
@@ -2,10 +2,10 @@
 %%All the values are takes from their respective data sheets
 
 L=150e-6; %inductance value
-C=2.2e-6; %capacitor size
-VoltageDrop=0; %drop across diode
-R_on=0.; %internal R of device
+C=220e-6; %capacitor size
+VoltageDrop=1.35; %drop across diode
+R_on=0.160; %internal R of device
 WidthP=60 %PWM value
-R_diode=0.000001;
+R_diode=0.001;
 SnubberR=250;
 SnubberC=100e-9;

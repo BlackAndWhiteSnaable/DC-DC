d33a4610c56f01b5895c81e8ff9d6829cfc85b39
Ivan Kulin <ivankulin@gmail.com>
2018-12-12 16:32:02 +0100

    Fix front page etc

--- Figure_conv2.m
+++ Figure_conv2.m
@@ -20,14 +20,3 @@ title('$V_C$','Interpreter', 'latex','fontsize',12)
 xlabel('Time[$s$]', 'Interpreter', 'latex','fontsize',12);
 ylabel('Voltage[$V$]', 'Interpreter', 'latex','fontsize',12);
 leg = legend('V_C = V_o');
-
-%Change Name of variable of whatever you set it in Simulink
-subplot(4,1,3)
-plot(I_out,'Color',[1,0,0])
-axis([0.37724 0.377282 0.602 0.61]) 
-%%Set title, labels, legend labels
-title('$I_o$','Interpreter', 'latex','fontsize',12)
-xlabel('Time[$s$]', 'Interpreter', 'latex','fontsize',12);
-ylabel('Current[$A$]', 'Interpreter', 'latex','fontsize',12);
-leg = legend('I_o');
-
